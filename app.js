be9833f777814733b891e44b48f1034ea13a0e79
lavender <id0421942@gmail.com>
2021-11-11 11:19:07 +0800

    feat:login route

--- app.js
+++ app.js
@@ -7,8 +7,15 @@ const PORT = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('login')
+})
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body
+    console.log(email, password)
 })
 
 app.listen(PORT, () => {
